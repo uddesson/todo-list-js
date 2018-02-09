f3a6a7afc926e74f8133c4c727c12a38663ccba1
Emma Uddesson <emma.uddesson@gmail.com>
2018-02-09 16:53:09 +0100

    Draft  (only comments) for functions

---
+++ main.js
@@ -0,0 +1,27 @@
+//--- Function drafts
+
+/*
+addTask() 
+Adds the input value (string) as a list-element in ul of incomplete tasks
+.. when you click the addButton
+Also needs a checkbox and a remove-button added to the specific task ..? 
+*/
+
+/* 
+removeTask()
+Removes the listed task itself (this) completely
+wether it's in the incomplete or complete-list
+Triggered by a remove-button of some kind
+*/
+
+/* markTaskComplete()
+Moves a task from the incomplete task list to the top of completed task-list
+This is triggered when the checkbox for that sepcific task is checked
+Sets a specific css class to the completed task as well, clearly marking it as completed/done
+*/
+
+/* 
+clearTasks()
+Clears (removes) all your tasks - from both lists
+when you click the clear button
+*/
